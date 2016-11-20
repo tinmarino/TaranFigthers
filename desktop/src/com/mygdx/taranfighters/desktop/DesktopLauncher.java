e94c4da6cbd4eece7316c4132165a970f8e54fff
tinmarino <tinmarino@gmail.com>
2016-11-20 20:23:48 +0100

    Projet generated for gdx1.5.5

---
+++ desktop/src/com/mygdx/taranfighters/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.taranfighters.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.taranfighters.MainGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGdx(), config);
+	}
+}
