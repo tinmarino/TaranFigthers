86d0f6feaf94899fa351ece145e7d24c73d4dc3d
tinmarino <tinmarino@gmail.com>
2016-12-17 16:36:13 +0100

    Add Zombie Images (1Mo)

--- core/src/com/mygdx/taranfighters/Zombie.java
+++ core/src/com/mygdx/taranfighters/Zombie.java
@@ -1,5 +1,6 @@
 package com.mygdx.taranfighters;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Zombie extends Character {
@@ -12,6 +13,9 @@ public class Zombie extends Character {
 
 
 	public void init(){
+	}
+
+	public static void addZombie(World world, Vector2 position){
 	}
 
 }
