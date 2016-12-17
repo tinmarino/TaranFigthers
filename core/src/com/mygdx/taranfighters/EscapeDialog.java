3d880a56d380fa5b466a32343d036c7a52e1c33c
tinmarino <tinmarino@gmail.com>
2016-12-17 13:14:54 +0100

    Dialog Better, Warning ugly commit with skin and textureAtlas.

--- core/src/com/mygdx/taranfighters/EscapeDialog.java
+++ core/src/com/mygdx/taranfighters/EscapeDialog.java
@@ -3,31 +3,82 @@ package com.mygdx.taranfighters;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 
 public class EscapeDialog extends Dialog {
-	public Skin skin;
+	public static Skin skin;
 
 
 	public EscapeDialog() {
-		super("escape dialog", getSkinPlease());
+		super("", getSkinPlease());
 		G.log("EscapeDialog : called");
+		this.padTop(getPrefHeight()/5);
+
 		text("What you wanna do ?");
-		button("Continue of course.", WHAT.CONTINUE);
-		button("ChangeLevel, please ...", WHAT.LEVEL);
-		//button("Leave you now !", WHAT.EXIT);
+
+		// CONTINUE 
+		TextButton btn1 = new TextButton("Continue of course.", skin.get("iul", TextButtonStyle.class));
+		this.setObject(btn1, WHAT.CONTINUE);
+		this.getButtonTable().add(btn1).height(getPrefHeight()/5);
+		this.getButtonTable().row();
+
+		// LEVEL SWITCH 
+		TextButton btn2 = new TextButton("ChangeLevel, please ...", skin.get("jak", TextButtonStyle.class));
+		this.setObject(btn2, WHAT.LEVEL); 
+		this.getButtonTable().add(btn2).height(getPrefHeight()/5);
+		this.getButtonTable().row();
+
+		// EXIT 
+		TextButton btn3 = new TextButton("Leave you now !", skin.get("roz", TextButtonStyle.class));
+		this.setObject(btn3, WHAT.EXIT);
+		this.getButtonTable().add(btn3).height(getPrefHeight()/5);
+		this.getButtonTable().row();
 	}
 	
 	@Override
 	protected void result(Object object){
 		G.log("Escape Dialog is " + object);
+
+		// EXIT 
+		if (object == WHAT.EXIT){
+			G.log("Escape Dialog : Bye");
+			Gdx.app.exit();
+			return;
+		}
+
+		// CONTINUE
+		if (object == WHAT.CONTINUE){
+			G.log("Escape Dialog : Continue");
+			return;
+		}
+
+		// LEVEL 
+		if (object == WHAT.LEVEL){
+			G.log("Escape Dialog : Change Level");
+			G.game.setScreen(new ChooseScreen());
+			return;
+		}
 	}
+
+    @Override
+    public float getPrefWidth() {
+        return Gdx.graphics.getWidth()/2;
+    }
+
+    @Override
+    public float getPrefHeight() {
+        return Gdx.graphics.getWidth()/2;
+    }
 
 	public enum WHAT{CONTINUE, LEVEL, EXIT};
 
 
 	public static Skin getSkinPlease(){
-		return new Skin(Gdx.files.internal("skin/escape/escape.json"));
+		skin = new Skin(Gdx.files.internal("skin/escape/escape.json"));
+		skin.getFont("escape-font").getData().setScale(0.5f,0.5f);
+		return skin;
 	}
 
 }
--- desktop/src/com/mygdx/taranfighters/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/taranfighters/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.mygdx.taranfighters.desktop;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.taranfighters.G;
@@ -10,6 +12,10 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 480 *2;
 		config.height = 320 *2;
+		config.title = "Taran Fighters";
+		config.addIcon("bck/taran_icon32.png", Files.FileType.Internal) ;
+		config.addIcon("bck/taran_icon16.png", Files.FileType.Internal) ;
+		
 		G.midiPlayer = new DesktopMidiPlayer();
 		new LwjglApplication(new MainGdx(), config);
 	}
