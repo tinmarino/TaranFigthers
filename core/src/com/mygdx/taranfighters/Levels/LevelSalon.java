903dde4ca2138b8d524d78c3f92f47f8396fbadc
tinmarino <tinmarino@gmail.com>
2017-01-24 19:45:28 +0100

    Add Zombies to LevelSalon. Goal : Kill all zombies.

--- core/src/com/mygdx/taranfighters/Levels/LevelSalon.java
+++ core/src/com/mygdx/taranfighters/Levels/LevelSalon.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.taranfighters.Level;
+import com.mygdx.taranfighters.Character;
 
 public class LevelSalon extends Level {
 	Music music1, music2;
@@ -14,7 +15,27 @@ public class LevelSalon extends Level {
 
 		music1 = Gdx.audio.newMusic(Gdx.files.internal("music/quote/it_is_always.mp3")); 
 		music1.play();
+
+
+		makeZombie(7, 30).fromTo(5f, 9f); 
+		makeZombie(33, 20).fromTo(26, 42); 
+		makeZombie(27, 14).fromTo(25, 28); 
+		makeZombie(78, 30).fromTo(76, 84); 
+		makeZombie(65, 10).fromTo(65, 83); 
+		makeZombie(88, 07).fromTo(88, 117); 
+		makeZombie(110, 25).fromTo(99, 110); 
 	}
+
+	@Override
+	public boolean isLevelFinished(Character char1){
+		// Victory 
+		if (charList.size() == 0){
+			this.finishedEnum = FINISHED.VICTORY;
+			return true;
+		}
+		return false;
+	}
+
 
 
 	@Override
