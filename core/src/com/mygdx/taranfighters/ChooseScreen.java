fe6c6a0b3c637c51303d1a32769ede41f0d7e10f
tinmarino <tinmarino@gmail.com>
2017-01-26 21:20:42 +0100

    ChooseMenu add background Iamge. TODO, ugly ?

--- core/src/com/mygdx/taranfighters/ChooseScreen.java
+++ core/src/com/mygdx/taranfighters/ChooseScreen.java
@@ -10,8 +10,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
@@ -164,6 +166,12 @@ public class ChooseScreen implements Screen {
 
 		ScrollPane scrollPane = new ScrollPane(table);
 		scrollPane.setFillParent(true);
+
+		Skin skin = EscapeDialog.getSkinPlease();
+		Image image = new Image(skin.getDrawable("taran_icon1"));
+		image.setFillParent(true);
+
+		stage.addActor(image);
 		stage.addActor(scrollPane);
 	}
 
