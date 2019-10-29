a13edea7747f86b77765039bffc1fe3f2e985894
tinmarino <tinmarino@gmail.com>
2019-10-29 14:48:09 +0100

    Tipo

--- core/src/com/mygdx/taranfighters/EscapeDialog.java
+++ core/src/com/mygdx/taranfighters/EscapeDialog.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.utils.Disposable;
 
 
-public class EscapeDialog extends Dialog  implements Disposable{
+public class EscapeDialog extends Dialog implements Disposable{
 	public static Skin skin;
 	public boolean isDestroyed = false;
 	public Music music;
